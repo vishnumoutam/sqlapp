4fb5bc5966603070d7f5a61d7739d1e8c88022a1
VishnuMurthy Moutam <VishnuMurthy.Moutam@lntinfotech.com>
2022-12-01 12:18:24 +0530

    Added project

---
+++ SQLApp/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using sqlapp.Models;
+using sqlapp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+
+        }
+    }
+}
