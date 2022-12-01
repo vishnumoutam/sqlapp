3d6a6201a44f961d384de519a1c488c0db9173bc
vishnumoutam <81789715+vishnumoutam@users.noreply.github.com>
2022-12-01 12:13:27 +0530

    Delete sqlapp directory
    deleted

--- sqlapp/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace sqlapp.Models
-{
-    public class Product
-    {
-        public int ProductID { get; set; }
-        public string ProductName { get; set; }    
-        public int Quantity { get; set; }
-    }
-}
